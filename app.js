83f09220f8605a9944b2af0411ac5e87017c26e4
Nashira Emery <nashiraemery1@gmail.com>
2018-03-03 16:01:45 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+//Budget Controller
+var budgetController = (function() {
+
+      // some code
+
+})();
+
+//UI Controller
+var UIController = (function() {
+
+    // Some code
+
+})();
+
+
+//Global app controller
+var controller = (function(budgetCtrl, UICtrl) {
+
+    document.querySelector('')
+
+})(budgetController, UIController);
